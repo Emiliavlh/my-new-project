3e9a377f4c113dcb11f82773d54f6f306742948f
Emiliavlh <eaa25evlh@students.eaaa.dk>
2025-09-24 14:11:58 +0200

    Final product

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-document.body.innerHTML += "Hello JavaScript!";
-// alert("Hello Dat.js");
-console.log("Hello Multimedia Designers");
-console.log("Hi Students!");
-console.log("This is your first JavaScript App 🎉");
 function sayHello() {
 document.querySelector("h3").textContent =
-  "Hej " + document.querySelector("input").value;
+  "Hej " + document.querySelector("input").value;   
 }
+
 document.querySelector("button").addEventListener("click", sayHello);
 
+function sayAge() {
+
+    document.querySelector("h4").textContent =
+  "Du er " + document.querySelector("#age").value + " år gammel";
+
+}
+
+document.querySelector("#alder").addEventListener("click", sayAge);
+
+function sayEmail() {
+    document.querySelector("h5").textContent = "Din email er " + document.querySelector("#email").value; 
+}
+
+document.querySelector("#gmail").addEventListener("click", sayEmail);
